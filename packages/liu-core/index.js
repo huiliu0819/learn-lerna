40ff191ef4073dfd5297ba0fc562facd8b044d78
liuhui <liuhui@pinming.cn>
2021-05-27 17:22:55 +0800

    test publish

---
+++ packages/liu-core/index.js
@@ -0,0 +1,8 @@
+const { API } = require('hui-core')
+const axios = require('axios')
+
+const getImg = () => axios.get(API)
+
+module.exports = getImg
+
+getImg().then(res => console.log(res.data.length))
